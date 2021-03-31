df417b2629a99fc3781af525401a5482ce3763ba
Valerii_kor <myksv200@gmail.com>
2021-03-31 21:21:06 +0400

    Map

--- KPI_Lab/DataBaseManager.cs
+++ KPI_Lab/DataBaseManager.cs
@@ -1,7 +1,23 @@
-﻿namespace KPI_Lab
+﻿using System.Collections.Generic;
+
+namespace KPI_Lab
 {
     public class DataBaseManager
     {
+        private string _dataBaseRef;
+        private List<Parking> _parkings;
+
+        public DataBaseManager(string dataBaseRef, List<Parking> parkings)
+        {
+            _dataBaseRef = dataBaseRef;
+            _parkings = parkings;
+        }
+
+        public void GetParkings(Parking parking)
+        {
+            _parkings.Add(parking);
+        }
         
+        public void UpdateInformation () {}
     }
 }
--- KPI_Lab/Map.cs
+++ KPI_Lab/Map.cs
@@ -2,6 +2,25 @@
 {
     public class Map
     {
+        private int[] _currentLocation;
+        private Parking _destination;
+        private bool _isArrived;
+        private Booking _booking;
+        private DataBaseManager _DBmanager;
         
+        private void BuildRoute(){}
+        private void UpdateLocation(){}
+        private void ShowParkings(){}
+
+        public Parking DestinationRequest()
+        {
+            return null;
+        }
+
+        public Parking ChooseParking()
+        {
+            return null;
+        }
+
     }
 }
